43ba2e34b7a6736912b5b979cca1ad80bbc221ce
Mohamed Debbagh <moha7108@protonmail.com>
2024-02-12 14:09:53 -0500

    first implementation of motor abraction

--- include/Motor.hpp
+++ include/Motor.hpp
@@ -19,24 +19,64 @@ class StepperMotor : public Motor {
         }
 
 
-        void set_speed(){
+        void action(float speed, int steps, bool direction){
+            reset_activity();
+            set_speed(speed);
+            driver->set_direction(direction);
+            set_steps(steps);
+        }
+
+
+        void set_speed(float revs_per_second){
+
+            delay_per_pulse = (revs_per_second / steps_per_rev) * 1000000; // microseconds per step
 
         }
+
+        bool step(){
+            if (queued_steps != 0){
+                driver -> step_pulse();
+                update_step();
+
+                return true;
+            }else{
+                return false;
+            }
+            
+        }
+
+        // void action_reader(){}
 
 
     private:
 
         unsigned int  steps_per_rev;
         StepperMotorDriver* driver;
-        int queued_steps;
-        int steps_left;
-        float delay_for_speed;
+        int queued_steps = 0;
+        int step_tracker = 0;
+        float rev_tracker = 0;
+        // int steps_left;
+        double delay_per_pulse = 5000; //microseconds
+        float steps_per_second;
+        float mm_per_rev;
 
-        int revs_to_steps(int revs){
-            
+        void reset_activity(){
+            queued_steps=0;
+            step_tracker=0;
+            rev_tracker=0;
         }
 
-    
+        void set_steps(int steps){
+            queued_steps = steps;
+        }
+
+        void update_step(){
+            queued_steps--;
+            step_tracker++;
+        }
+
+
+
     
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 #include "StepperMotorDriver.hpp"
+#include "Motor.hpp"
 #include "pico/stdlib.h"
 #include <string>
 #include <memory>
 
 using namespace std;
 
-struct StepperMotor{
-    StepperMotorDriver* motor_driver; // pointer to (&obj) instance of a child of the StepperMotorDriver Class
-    int steps_per_rev;
-    string name;
+// struct StepperMotor{
+//     StepperMotorDriver* motor_driver; // pointer to (&obj) instance of a child of the StepperMotorDriver Class
+//     int steps_per_rev;
+//     string name;
 
-    // StepperMotor Constructor
-    StepperMotor(StepperMotorDriver* md, int steps_per_rev = 200, string name = "SM1") 
-    : motor_driver(md), steps_per_rev(steps_per_rev), name(name){}
-};
+//     // StepperMotor Constructor
+//     StepperMotor(StepperMotorDriver* md, int steps_per_rev = 200, string name = "SM1") 
+//     : motor_driver(md), steps_per_rev(steps_per_rev), name(name){}
+// };
 
 
 int main(){
@@ -22,20 +23,26 @@ int main(){
 
     TB67S128FTG md1(0, 1, 2, 3, 4, 5);
     
-    StepperMotor stepper1(&md1);
+    StepperMotor stepper1(&md1, 200);
+
+    stepper1.action(1, 1000, true);
     
 
     while (true){
 
-        for(int i=0; i<1000; i++){
-        stepper1.motor_driver -> step_pulse(true);
-        sleep_us(5000);
-        }
+        stepper1.step();
 
-        for(int i=0; i<1000; i++){
-        stepper1.motor_driver -> step_pulse(false);
         sleep_us(5000);
-        }
+
+        // for(int i=0; i<1000; i++){
+        // stepper1.motor_driver -> step_pulse(true);
+        // sleep_us(5000);
+        // }
+
+        // for(int i=0; i<1000; i++){
+        // stepper1.motor_driver -> step_pulse(false);
+        // sleep_us(5000);
+        // }
     }
     
 };
