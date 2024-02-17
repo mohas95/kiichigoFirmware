16d7f4fcc64310796d27393c67e69ecf81bbbbb1
Mohamed Debbagh <moha7108@protonmail.com>
2024-02-17 18:39:50 -0500

    add min speed and max speed handling

--- include/Motor.hpp
+++ include/Motor.hpp
@@ -14,11 +14,8 @@ class StepperMotor : public Motor{
     // revolutions per second
 
     public:
-        StepperMotor(StepperMotorDriver* driver, int steps_per_rev) 
-        : driver(driver), steps_per_rev(steps_per_rev){
-
-        }
-
+        StepperMotor(StepperMotorDriver* driver, int steps_per_rev, int min_speed = 1, int max_speed = 3) 
+        : driver(driver), steps_per_rev(steps_per_rev), min_speed(min_speed), max_speed(max_speed){}
 
         void action(float speed, int steps, bool direction) override{
             reset_activity();
@@ -27,8 +24,13 @@ class StepperMotor : public Motor{
             set_steps(steps);
         }
 
-
         void set_speed(float revs_per_second){
+
+            if (revs_per_second >= max_speed){
+                revs_per_second = max_speed;
+            } else if(revs_per_second <= min_speed){
+                revs_per_second = min_speed;
+            }
 
             delay_per_pulse =  1000000 / (steps_per_rev*revs_per_second ); // microseconds per step
 
@@ -43,7 +45,6 @@ class StepperMotor : public Motor{
             }else{
                 return false;
             }
-            
         }
 
         int get_delay_per_pulse(){
@@ -51,6 +52,12 @@ class StepperMotor : public Motor{
         }
 
         int get_delay_per_pulse(float revs_per_second){
+
+            if (revs_per_second >= max_speed){
+                revs_per_second = max_speed;
+            } else if(revs_per_second <= min_speed){
+                revs_per_second = min_speed;
+            }
             
             return 1000000 / (steps_per_rev*revs_per_second ); // microseconds per step
         }
@@ -67,7 +74,10 @@ class StepperMotor : public Motor{
         // int steps_left;
         int delay_per_pulse = 5000; //microseconds
         float steps_per_second;
-        float mm_per_rev;
+
+        int min_speed;
+        int max_speed;
+        // float mm_per_rev;
 
         void reset_activity(){
             queued_steps=0;
--- src/main.cpp
+++ src/main.cpp
@@ -32,23 +32,42 @@ int main(){
     Scheduler motor_scheduler;
     TB67S128FTG md1(0, 1, 2, 3, 4, 5);
     
-    StepperMotor stepper1(&md1, 200);
+    StepperMotor stepper1(&md1, 200, 1, 3);
 
-    Task task1 = create_stepper_task(stepper1, 1,1000, true);
-    Task task2  = create_stepper_task(stepper1, 2, 1000, false); 
-    Task task3  = create_stepper_task(stepper1, 3, 1000, false);    
-    Task task4  = create_stepper_task(stepper1, 4, 1000, false);    
+    Task task1 = create_stepper_task(stepper1, 1,200, true);
+    // Task task2  = create_stepper_task(stepper1, 2, 1000, false); 
+    // Task task3  = create_stepper_task(stepper1, 3, 1000, true);    
+    // Task task4  = create_stepper_task(stepper1, 4, 1000, false);
+    // Task task5 = create_stepper_task(stepper1, 5,1000, true);
+    // Task task6  = create_stepper_task(stepper1, 6, 1000, false); 
+    // Task task7  = create_stepper_task(stepper1, 7, 1000, true);    
+    // Task task8  = create_stepper_task(stepper1, 8, 1000, false);   
+    // Task task9  = create_stepper_task(stepper1, 9, 1000, true);    
+    // Task task10  = create_stepper_task(stepper1, 10, 1000, false); 
 
 
     motor_scheduler.add_task(task1);
     motor_scheduler.run();
-
-    motor_scheduler.add_task(task2);
-    motor_scheduler.run();
-    motor_scheduler.add_task(task3);
-    motor_scheduler.run();
-    motor_scheduler.add_task(task4);
-    motor_scheduler.run();
+    // motor_scheduler.add_task(task2);
+    // motor_scheduler.run();
+    // motor_scheduler.add_task(task3);
+    // motor_scheduler.run();
+    // motor_scheduler.add_task(task4);
+    // motor_scheduler.run();
+    // motor_scheduler.add_task(task5);
+    // motor_scheduler.run();
+    // motor_scheduler.add_task(task6);
+    // motor_scheduler.run();
+    // motor_scheduler.add_task(task7);
+    // motor_scheduler.run();
+    // motor_scheduler.add_task(task8);
+    // motor_scheduler.run();
+    // motor_scheduler.add_task(task9);
+    // motor_scheduler.run();
+    // motor_scheduler.add_task(task10);
+    // motor_scheduler.run();
+    
+    
 
     return 0;
 };
