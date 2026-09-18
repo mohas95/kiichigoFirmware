eec64be3ff3102637e8091c22d357c4a6c2e01ad
mohas95 <moha.debbagh95@gmail.com>
2026-09-18 18:58:48 -0400

    update test and success

--- src/test.cpp
+++ src/test.cpp
@@ -20,26 +20,35 @@ int main(){
     // Print a message to the USB serial
     printf("USB Serial connected!\n");
 
-    TB67S128FTG stepper_driver1(6, 7, 8, 9, 10, 11, StepperDriver::StepMode::FULL);
+    TB67S128FTG stepper_driver1(6, 7, 8, 9, 10, 11, StepperDriver::StepMode::QUARTER);
     // LimitSwitch home_switch("home", 18, 0, {"z"}, 1, LimitSwitch::PullMode::PULL_UP);
     StepperMotor stepper1("z", stepper_driver1, 200, 50);
 
-    // stepper1.step_for(1000, 50);
+    stepper1.step_for(1000, 100);
 
-    // uint32_t counter = 0;
+    uint32_t counter = 0;
 
-    // while (counter <= 2000){
-    //      printf("%u\n", counter);
-    //      counter++;
-    //     sleep_ms(100);
+    while (counter <= 200){
+         printf("%u\n", counter);
+         counter++;
+        sleep_ms(100);
 
-    // }
-
-    while (true) {
-        stepper_driver1.step_high();
-        sleep_us(50);
-
-        stepper_driver1.step_low();
-        sleep_ms(50);
     }
+
+    stepper1.step_for(-10000, 50);
+    counter = 0;
+
+    while (counter <= 200){
+         printf("%u\n", counter);
+         counter++;
+        sleep_ms(100);
+
+    }
+    // while (true) {
+    //     stepper_driver1.step_high();
+    //     sleep_us(50);
+
+    //     stepper_driver1.step_low();
+    //     sleep_ms(50);
+    // }
 }
